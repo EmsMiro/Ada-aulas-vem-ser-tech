ff347e8bdeb70678bff32ba4cf71a5765e21b2f5
EmsMiro <ednirlicosta@gmail.com>
2023-10-14 07:44:22 -0300

    alterando style.css e adicionando validações ao main.js

--- Aula 3/eventoOng/main.js
+++ Aula 3/eventoOng/main.js
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', function() {
             categoriaInscricao,
             valorDoacao
         };
-
+        
         // enviando os dados para o json-server
         fetch(dbURL, {
             method: 'POST',
@@ -30,19 +30,67 @@ document.addEventListener('DOMContentLoaded', function() {
             },
             body: JSON.stringify(dadosParaEnviar)
         })
-        .then(response => response.json())
+        
+        .then(response => {
+            return response.json(); // Converte a resposta para JSON
+        })
         .then(data => {
-            // mostrando a mensagem na tela
+            // Agora você pode acessar os dados corretamente
             const mensagem = `Inscrição confirmada! Atleta ${data.nome}, ${data.idade} anos, email: ${data.email}, tamanho camiseta: ${data.tamanhoCamiseta}, Categoria: ${data.categoriaInscricao}, Doou o valor de ${data.valorDoacao}`;
             resultadoDiv.textContent = mensagem;
         })
         .catch(error => {
             console.error('Erro ao enviar os dados para o servidor JSON:', error);
-        });   
-
-        // lembre -> essa parte do código foi pra testar!
-        // const mensagem = `Inscrição confirmada! Atleta ${nome}, ${idade} anos, email: ${email}, tamanho camiseta: ${tamanhoCamiseta}, Categoria: ${categoriaInscricao}, Doou o valor de ${valorDoacao}`;
-        // resultadoDiv.textContent = mensagem;
-        
+        });        
     })
 })
+
+//Constantes para lidar com as validações
+const inputName = document.querySelector('#input-name');
+const inputEmail = document.querySelector('#input-email');
+const inputDoacao = document.querySelector('#input-doacao');
+
+
+inputName.addEventListener('keypress', (e) => {
+    const keyCode = (e.keyCode ? e.keyCode : e.which);
+    const regex = /[A-Za-zÁáÉéÍíÓóÚúÃãẼẽĨĩÕõŨũÑñÜüÇç~]/;
+    //47 ao 58 são números
+    //para provar que o que está sendo pressionado são números basta descomentar a linha abaixo
+    // console.log(keyCode)    
+    if (keyCode >= 47 && keyCode <= 58) {
+        e.preventDefault();
+    } else if (!regex.test(String.fromCharCode(keyCode))) {
+        e.preventDefault();
+    }
+});
+
+// inputEmail.addEventListener('blur', function () {
+//     const email = emailInput.value;
+//     const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+//     if (!emailRegex.test(email)) {
+//         alert('Por favor, insira um endereço de e-mail válido.');
+//         emailInput.value = ''; // Limpa o campo se o e-mail for inválido
+//     }
+// } );
+
+
+inputDoacao.addEventListener('input', function() {
+
+    const valorDigitado = inputDoacao.value;
+
+    if (valorDigitado <= 0 || valorDigitado === 0) {
+        // Define uma mensagem de erro
+        inputDoacao.setCustomValidity('Insira um valor válido (maior que zero).');
+    } else {
+        // Limpa a mensagem de erro
+        inputDoacao.setCustomValidity('');
+    }
+
+    // const valorNaoFormatado = valorDigitado;
+    // const valorFormatado = (new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL'}).format(valorNaoFormatado));
+    // console.log(valorFormatado)
+
+})
+
+
