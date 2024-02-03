3201fa742870ddac31226cb724e930a920e48580
EmsMiro <ednirlicosta@gmail.com>
2024-02-02 22:24:44 -0300

    alterando section ofertas

--- Modulo 5/aula 1/src/offer-products.jsx
+++ Modulo 5/aula 1/src/offer-products.jsx
@@ -1,11 +1,87 @@
 import React from "react";
 
 const OfferProducts = () => {
-    return (
-        <div class='offer-container'>
-            
+  return (
+    <div class="card-container">
+      <div class="card">
+        <div class="img-container">
+          <img
+            src="https://img.freepik.com/vetores-gratis/modelo-de-capa-de-album-de-design-plano_23-2150562226.jpg?w=740&t=st=1706895637~exp=1706896237~hmac=dd6c1a0160c3575655c2ea54f43c0e5f4f3d518935912015f8d2b25e1b3e54a5"
+            class="vinil-img"
+          />
+          <img
+            src="https://img.icons8.com/external-nawicon-glyph-nawicon/50/fd5500/external-Discount-ecommerce-nawicon-glyph-nawicon.png"
+            class="discount-img"
+          />
         </div>
-    );
+
+        <div class="vinil-infos-container">
+          <h3 class="vinil-title"> Vinil Tecno Music </h3>
+          <p>Ano: 2001</p>
+          <p>Artista: Ipsum Lorem</p>
+          <p class="price-p fi">R$ 150,00</p>
+          <p class="price-p">RS 100,00</p>
+        </div>
+
+        <div class="buttons-container">
+          <button id="btn-comprar">COMPRAR</button>
+          <button id="btn-adicionar">ADICIONAR AO CARRINHO</button>
+        </div>
+      </div>
+      <div class="card">
+        <div class="img-container">
+          <img
+            src="https://img.freepik.com/vetores-gratis/modelo-de-capa-de-album-de-design-plano_23-2150562226.jpg?w=740&t=st=1706895637~exp=1706896237~hmac=dd6c1a0160c3575655c2ea54f43c0e5f4f3d518935912015f8d2b25e1b3e54a5"
+            class="vinil-img"
+          />
+          <img
+            src="https://img.icons8.com/external-nawicon-glyph-nawicon/50/fd5500/external-Discount-ecommerce-nawicon-glyph-nawicon.png"
+            class="discount-img"
+          />
+        </div>
+
+        <div class="vinil-infos-container">
+          <h3 class="vinil-title"> Vinil Tecno Music </h3>
+          <p>Ano: 2001</p>
+          <p>Artista: Ipsum Lorem</p>
+          <p class="price-p fi">R$ 150,00</p>
+          <p class="price-p">RS 100,00</p>
+        </div>
+
+        <div class="buttons-container">
+          <button id="btn-comprar">COMPRAR</button>
+          <button id="btn-adicionar">ADICIONAR AO CARRINHO</button>
+        </div>
+      </div>
+      <div class="card">
+        <div class="img-container">
+          <img
+            src="https://img.freepik.com/vetores-gratis/modelo-de-capa-de-album-de-design-plano_23-2150562226.jpg?w=740&t=st=1706895637~exp=1706896237~hmac=dd6c1a0160c3575655c2ea54f43c0e5f4f3d518935912015f8d2b25e1b3e54a5"
+            class="vinil-img"
+          />
+          <img
+            src="https://img.icons8.com/external-nawicon-glyph-nawicon/50/fd5500/external-Discount-ecommerce-nawicon-glyph-nawicon.png"
+            class="discount-img"
+          />
+        </div>
+
+        <div class="vinil-infos-container">
+          <h3 class="vinil-title"> Vinil Tecno Music </h3>
+          <p>Ano: 2001</p>
+          <p>Artista: Ipsum Lorem</p>
+          <p class="price-p fi">R$ 150,00</p>
+          <p class="price-p">RS 100,00</p>
+        </div>
+
+        <div class="buttons-container">
+          <button id="btn-comprar">COMPRAR</button>
+          <button id="btn-adicionar">ADICIONAR AO CARRINHO</button>
+        </div>
+      </div>
+     
+
+    </div>
+  );
 };
 
 export default OfferProducts;
