1e5d068048884a381561998e0e33281a38964458
EmsMiro <ednirlicosta@gmail.com>
2024-02-09 01:04:28 -0300

    alterando função para o form

--- Modulo 5/aula 3/blog-comments/src/components/commentsForm/index.jsx
+++ Modulo 5/aula 3/blog-comments/src/components/commentsForm/index.jsx
@@ -3,40 +3,40 @@ import React, { useState } from 'react';
 import ComentsComponent from '../comentsPost';
 
 const CommentForm = () => {
-  // const [commenterName, setCommenterName] = useState('');
-  // const [commentText, setCommentText] = useState('');
+  const [commenterName, setCommenterName] = useState('');
+  const [commentText, setCommentText] = useState('');
 
-  // const handleNameChange = (event) => {
-  //   setCommenterName(event.target.value);
-  // };
+  const handleNameChange = (event) => {
+    setCommenterName(event.target.value);
+  };
 
-  // const handleCommentChange = (event) => {
-  //   setCommentText(event.target.value);
-  // };
+  const handleCommentChange = (event) => {
+    setCommentText(event.target.value);
+  };
 
-  // const handleSubmit = (event) => {
-  //   event.preventDefault();
-  //   const currentDate = new Date().toLocaleDateString('en-US', {
-  //     year: 'numeric',
-  //     month: 'short',
-  //     day: 'numeric',
-  //   });
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const currentDate = new Date().toLocaleDateString('en-US', {
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric',
+    });
 
-  //   // Construindo o novo componente de comentário com os dados do formulário
-  //   const newCommentComponent = (
-  //     <ComentsComponent
-  //       commenterName={commenterName}
-  //       commentDate={currentDate}
-  //       commentText={commentText}
-  //     />
-  //   );
+    // Construindo o novo componente de comentário com os dados do formulário
+    const newCommentComponent = (
+      <ComentsComponent
+        commenterName={commenterName}
+        commentDate={currentDate}
+        commentText={commentText}
+      />
+    );
     
-  //   console.log('Novo comentário:', newCommentComponent);
+    console.log('Novo comentário:', newCommentComponent);
 
     // Limpando os campos do formulário após o envio
-  //   setCommenterName('');
-  //   setCommentText('');
-  // };
+    setCommenterName('');
+    setCommentText('');
+  };
 
   return (
     <form onSubmit={handleSubmit} className='form-container'>
