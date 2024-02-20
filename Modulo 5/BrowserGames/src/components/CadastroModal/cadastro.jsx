ede384ccffb7dbd74120ab6a90d1fa6dc8d7b99c
EmsMiro <ednirlicosta@gmail.com>
2024-02-20 08:19:50 -0300

    Cadastro de user finalizado e integrado ao db.json

--- Modulo 5/BrowserGames/src/components/CadastroModal/cadastro.jsx
+++ Modulo 5/BrowserGames/src/components/CadastroModal/cadastro.jsx
@@ -1,13 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./cadastro.css";
 
 const CadastroModal = ({ onClose }) => {
+    const [nome, setNome] = useState('');
+    const [idade, setIdade] = useState('');
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+
     const handleClose = () => {
         onClose();
     };
 
     const handleModalClick = (e) => {
         e.stopPropagation();
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const novoUsuario = {
+            name: nome,
+            idade: idade,
+            email: email,
+            password: senha
+        };
+
+        try {
+            const response = await fetch('http://localhost:3000/users', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(novoUsuario),
+            });
+
+            if (response.ok) {
+                alert('Usuário cadastrado com sucesso!');
+                onClose();
+            } else {
+                throw new Error('Erro ao cadastrar usuário.');
+            }
+        } catch (error) {
+            console.error('Erro:', error);
+            alert('Ocorreu um erro ao cadastrar o usuário. Por favor, tente novamente mais tarde.');
+        }
     };
 
     return (
@@ -17,22 +53,22 @@ const CadastroModal = ({ onClose }) => {
                     X
                 </span>
                 <h2>Cadastro</h2>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div>
                         <label>Nome</label>
-                        <input type="text" placeholder="Enzo da Silva" />
+                        <input type="text" placeholder="Insira seu nome completo" value={nome} onChange={(e) => setNome(e.target.value)} />
                     </div>
                     <div>
-                        <label>Data de Nascimento</label>
-                        <input type="date" placeholder="01/01/2001" />
+                        <label>Idade</label>
+                        <input type="text" placeholder="Insira sua idade" value={idade} onChange={(e) => setIdade(e.target.value)} />
                     </div>
                     <div>
                         <label>Email</label>
-                        <input type="email" placeholder="enzo@email.com" />
+                        <input type="email" placeholder="Seu melhor email" value={email} onChange={(e) => setEmail(e.target.value)} />
                     </div>
                     <div>
                         <label>Senha</label>
-                        <input type="password" placeholder="1234" />
+                        <input type="password" placeholder="Insira uma senha" value={senha} onChange={(e) => setSenha(e.target.value)} />
                     </div>
                     <button type="submit">Cadastrar</button>
                 </form>
