ca7e91956624c1015ddec77a826d5494fa10e5ee
EmsMiro <ednirlicosta@gmail.com>
2024-02-22 06:54:52 -0300

    enviando lógica de pesquisa finalizada e parcialmente funcional

--- Modulo 5/BrowserGames/src/components/Main/main.jsx
+++ Modulo 5/BrowserGames/src/components/Main/main.jsx
@@ -6,6 +6,7 @@ import SearchBar from '../SearchBar/searchBar';
 
 const Main = ()=> {
   const [games, setGames] = useState([]); 
+  const [filteredGames, setFilteredGames] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState(""); // variável de estado para armazenar o nome da categoria q foi selecionada 
   
   useEffect(() => {
@@ -42,13 +43,33 @@ const Main = ()=> {
     } catch (error) {
       console.error('Erro:', error.message);
     }
-  };    
+  };
+  
+  const handleSearch = async (searchTerm) => {
+    try {
+        // Buscando no server
+        const response = await fetch(`http://localhost:3000/games?title_like=${searchTerm}`);
+        if (!response.ok) {
+            throw new Error('Erro ao obter os dados');
+        }
+
+        // Convertendo a resposta para json
+        const data = await response.json();
+        setFilteredGames(data);
+
+        if (data.length === 0) {
+            alert("Esse jogo não foi encontrado no nosso banco de dados. :(");
+        }
+    } catch (error) {
+        console.error('Erro:', error.message);
+    }
+};
     return(
         <main>        
         <Aside handleItemClick={handleItemClick} />
         <div className="content-main-container">
-        <SearchBar />                     
-        <GamesContainer games={games} selectedCategory={selectedCategory} />
+        <SearchBar handleSearch={handleSearch} />                     
+        <GamesContainer games={filteredGames.length > 0 ? filteredGames : games} selectedCategory={selectedCategory} />
         </div>        
       </main>
     );
--- Modulo 5/BrowserGames/src/components/SearchBar/searchBar.jsx
+++ Modulo 5/BrowserGames/src/components/SearchBar/searchBar.jsx
@@ -2,39 +2,25 @@ import "./searchbar.css";
 import { useState } from "react";
 import Icon from "../IconsList/icon";
 
-const SearchBar = ({ setFilteredGames }) => {
+const SearchBar = ({ handleSearch }) => {
     const [searchTerm, setSearchTerm] = useState("");
 
-  const handleSearch = async () => {
-    try {
-      // buscando no server
-      const response = await fetch(`http://localhost:3000/games?title_like=${searchTerm}`);
-      if (!response.ok) {
-        throw new Error('Erro ao obter os dados');
-      }
+    const onClickSearch = async () => {
+        await handleSearch(searchTerm);
+    };
 
-      // lembre-se -> smp converter a resposta pra json
-      const data = await response.json();
-
-      // pra definir os jogos filtrados
-      setFilteredGames(data);
-      
-      
-      if (data.length === 0) {
-        alert("Esse jogo não foi encontrado no nosso banco de dados. :(");
-      }
-    } catch (error) {
-      console.error('Erro:', error.message);
-    }
-  };
   return (
     <section className="search-section">
       <h2 className="search-title">Pesquisar por nome do jogo:</h2>
       <label className="search-input-label">
-        <input type="text" name="search-bar" id="searchBar" value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)} />
+        <input 
+        type="text" 
+        name="search-bar" 
+        id="searchBar" 
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)} />
       </label>
-      <div className="icon-search" onClick={handleSearch}>
+      <div className="icon-search" onClick={onClickSearch}>
         <Icon name="search" />
       </div>
     </section>
