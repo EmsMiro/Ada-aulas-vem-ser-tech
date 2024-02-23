9dbbd7d70744182821697dead800bbf1f480156f
EmsMiro <ednirlicosta@gmail.com>
2024-02-23 15:36:21 -0300

    enviando lógica da funcionalidade do login finalizada

--- Modulo 5/BrowserGames/src/components/BrowserGamesProfile/browserGamesProfile.jsx
+++ Modulo 5/BrowserGames/src/components/BrowserGamesProfile/browserGamesProfile.jsx
@@ -1,11 +1,18 @@
 import "./browserGamesProfile.css";
 import DropDownMenu from "../DropDownMenu/dropDownMenu";
+import GamesContainer from "../GamesContainer/gamesContainer";
+import Card from "../CardGame/cardgame";
 
 const BrowserGamesProfile = () => {
   return (
-    <section className="browser-games-container">
-      <h2 className="browser-games-container-title">Seus Browser Games</h2>
-      <DropDownMenu />
+    <section>
+      <div className="browser-games-container">
+        <h2 className="browser-games-container-title">Seus Browser Games</h2>
+        <DropDownMenu />
+      </div>
+      <div className="teste-container">
+        <GamesContainer />        
+      </div>
     </section>
   );
 };
--- Modulo 5/BrowserGames/src/components/LoginModal/login.jsx
+++ Modulo 5/BrowserGames/src/components/LoginModal/login.jsx
@@ -1,34 +1,58 @@
 import "./login.css";
+import { useState } from "react";
 
 const LoginModal = ({ onClose }) => {
-    const handleClose = () => {
-        onClose();
-    };
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const handleModalClick = (e) => {
-        e.stopPropagation();
-    };
+  const handleClose = () => {
+    onClose();
+  };
 
-    return (
-        <div className="modal-container" onClick={handleClose}>
-            <div className="modal-content login-modal" onClick={handleModalClick}>
-                <span className="close-button" onClick={handleClose}>X</span>
-                <h2>Login</h2>
-                <form>
-                    
-                </form>
-                <div>
-                    <label>Email</label>
-                    <input type="text" placeholder="jose@email.com" />
-                </div>
-                <div>
-                    <label>Senha</label>
-                    <input type="password" placeholder="****" />
-                </div>
-                <button type="submit">Login</button>
-            </div>
-        </div>
-    );
+  const handleModalClick = (e) => {
+    e.stopPropagation();
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    //  requisição p/ verificar as credenciais
+    try {
+        const response = await fetch('http://localhost:3000/users?email=' + email);
+        const userData = await response.json();
+
+        if (userData.length === 1 && userData[0].password === password) {            
+            alert(`Login efetuado com sucesso! Bem-vindo ${userData[0].name}`);
+            handleClose(); // fechar o modal de login automaticamente
+        } else {
+            console.log('Credenciais inválidas');
+        }
+    } catch (error) {
+        console.error('Erro ao fazer login:', error);
+    }
+};
+
+  return (
+    <div className="modal-container" onClick={handleClose}>
+      <div className="modal-content login-modal" onClick={handleModalClick}>
+        <span className="close-button" onClick={handleClose}>
+          X
+        </span>
+        <h2>Login</h2>
+        <form onSubmit={handleLogin}>
+          <div>
+            <label>Email</label>
+            <input type="text" placeholder="jose@email.com" value={email} onChange={(e)=> setEmail(e.target.value)}/>
+          </div>
+          <div>
+            <label>Senha</label>
+            <input type="password" placeholder="****" value={password} onChange={(e) => setPassword(e.target.value)}/>
+          </div>
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default LoginModal;
